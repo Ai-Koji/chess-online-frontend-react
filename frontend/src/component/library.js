e251cf3e478e8720f19a5fe18c0e4f25ee0867c6
Ai-Koji <kireev.ivan.8@gmail.com>
2024-02-24 18:51:27 +0700

    feat: added fetch for library

--- frontend/src/component/library.js
+++ frontend/src/component/library.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/library-main.css';
 
 class Library extends React.Component {
+	// books: [{id, header, bookList: [{id, title, author, imageUrl}]}]
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -9,12 +10,34 @@ class Library extends React.Component {
 		};
 	}
 
+	getBooks = () => {
+		fetch('/api/library/main', { method: 'GET' })
+			.then((response) => response.json())
+			.then((result) => {
+				this.setState({ books: result });
+			})
+			.catch((result) => {
+				console.log(result);
+			});
+	};
+
+	componentDidMount() {
+		this.getBooks();
+	}
+
 	Books(blockIndex) {
 		let books = [];
-		for (let index = 0; index < this.state.books[blockIndex].bookList.length; index++) {
+		for (
+			let index = 0;
+			index < this.state.books[blockIndex].bookList.length;
+			index++
+		) {
 			books.push(
-				<a className="book" href="" key={index}>
-					<img src={this.state.books[blockIndex].bookList[index].imageUrl} alt="" />
+				<a className="book" href={`/library/book/${this.state.books[blockIndex].bookList[index].id}`} key={index}>
+					<img
+						src={this.state.books[blockIndex].bookList[index].imageUrl}
+						alt=""
+					/>
 					<div className="name-and-author">
 						<h5>{this.state.books[blockIndex].bookList[index].title}</h5>
 						<h6>{this.state.books[blockIndex].bookList[index].author}</h6>
@@ -32,10 +55,8 @@ class Library extends React.Component {
 			bookBlocks.push(
 				<div className="content-block" key={index}>
 					<h3>{this.state.books[index].header}</h3>
-					<div className="books">
-						{this.Books(index)}
-					</div>
-					<button className="showMore">показать еще</button>
+					<div className="books">{this.Books(index)}</div>
+					{/* <button className="showMore">показать еще</button> */}
 				</div>
 			);
 		}
@@ -49,9 +70,7 @@ class Library extends React.Component {
 					<h1>Шахматная Библия</h1>
 					<input placeholder="Поиск" className="search-input" />
 				</div>
-				<div className="content">
-					{this.BookBlocks()}
-				</div>
+				<div className="content">{this.BookBlocks()}</div>
 			</div>
 		);
 	}
