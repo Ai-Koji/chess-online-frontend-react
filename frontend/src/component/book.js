7b924eb2ead79ecfce61f4696e22cf017075f865
Ai-Koji <kireev.ivan.8@gmail.com>
2024-02-24 22:34:33 +0700

    feat: added fetch for book page

--- frontend/src/component/book.js
+++ frontend/src/component/book.js
@@ -2,30 +2,91 @@ import React from 'react';
 import '../styles/library-book.css';
 
 class Book extends React.Component {
+	constructor(props) {
+		const currentUrl = window.location.href;
+		const parts = currentUrl.split('/');
+		const bookId = parseInt(parts[parts.length - 1]);
+
+		super(props);
+		this.state = {
+			bookInfo: {},
+			bookId: bookId
+		};
+	}
+
+	getBookInfo = () => {
+		fetch(`http://127.0.0.1:3001/api/library/book/${this.state.bookId}`, {
+			method: 'GET'
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				this.setState({ bookInfo: result[0] });
+			})
+			.catch((result) => {
+				console.log(result);
+			});
+	};
+
+	componentDidMount() {
+		this.getBookInfo();
+	}
+
+	whereToBuy = () => {
+		let body = [];
+
+		// [{title, url}, {title, url}...]
+		let json = this.state.bookInfo.whereToBuy;
+
+		if (json) {
+			json = JSON.parse(json);
+			for (let index = 0; index < json.length; index++) {
+				body.push(<a href={json[index].url}>{json[index].title}</a>);
+			}
+		}
+
+		return body;
+	};
+
+	readPdf = () => {
+		if (this.state.bookInfo.pdfUrl) {
+			return (
+				<a className="read-pdf" href={this.state.bookInfo.pdfUrl} alt="">
+					Читать
+				</a>
+			);
+		} else {
+			return (
+				<a
+					className="read-pdf cant-read"
+					alt="защищено авторским правом/отсутствует"
+				>
+					Недоступно
+				</a>
+			);
+		}
+	};
+
 	render() {
 		return (
 			<div className="container" style={{ display: 'block' }}>
 				<div className="content">
-                    <div className="image">
-                        <img src={"https://i3.mybook.io/p/x378/book_covers/06/83/0683b0e6-5204-48ed-9dac-d62cb80fb27a.jpg"}/>
-                    </div>
-                    <div className="about-book">
-                        <h1>Руководство богатого папы по инвестированию</h1>
-                        <h6 className="author">Роберт Кийосаки</h6>
-                        <h3>О книге</h3>
-                        <p>Вопросы социальной психологии, активной жизненной позиции человека, социологии финансов складываются в генеральную тему, к которой автор обращался неоднократно – в книгах «Богатый папа, бедный папа» и «Квадрант денежного потока», уже знакомых широкому кругу читателей.
+					<div className="image">
+						<img src={this.state.bookInfo.imageUrl} />
+					</div>
+					<div className="about-book">
+						<h1>{this.state.bookInfo.title}</h1>
+						<h6 className="author">{this.state.bookInfo.author}</h6>
+						<h3>О книге</h3>
+						<p>{this.state.bookInfo.about}</p>
 
-                            Для широкого круга читателей.
+						{this.readPdf()}
 
-                            читайте онлайн полную версию книги «Руководство богатого папы по инвестированию» автора Роберт Кийосаки на сайте электронной библиотеки MyBook.ru. Скачивайте приложения для iOS или Android и читайте «Руководство богатого папы по инвестированию» где угодно даже без интернета. 
-                        </p>
-                        <a className="read-pdf cant-read">Читать бесплатно</a>
-                        <div className='where-to-buy'>
-                            <h3>Где купить:</h3>
-                                <a href="">Wildberries</a>
-                        </div>
-                    </div>
-                </div>
+						<div className="where-to-buy">
+							<h3>Где купить:</h3>
+							{this.whereToBuy()}
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
